24ea0fb3ba26ee6258dbf12eaa9caabd5c83008e
Ran Tavory <rantav@gmail.com>
2013-12-24 10:30:22 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README') as file:
     long_description = file.read()
 
 setup(name='flask-restful-swagger',
-      version='0.5',
+      version='0.6',
       url='https://github.com/rantav/flask-restful-swagger',
       packages=['flask_restful_swagger'],
       description='Extrarct swagger specs from your flast-restful project',
