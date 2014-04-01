99f02a195d4e0722a439bab62559b125045348b9
Andreas Richter <Andy.Richter@brige-way.com>
2014-04-01 13:35:40 -0400

    Correctly include package_data in setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ setup(name='flask-restful-swagger',
       zip_safe=False,
       packages=['flask_restful_swagger'],
       package_data= {
-        'flask_restful_swagger.static': [
-          'css/*.*',
-          'images/*.*',
-          'lib/*.*',
-          'lib/shred/*.*',
+        'flask_restful_swagger': [
+          'static/*.*',
+          'static/css/*.*',
+          'static/images/*.*',
+          'static/lib/*.*',
+          'static/lib/shred/*.*',
         ]
       },
       description='Extrarct swagger specs from your flast-restful project',
