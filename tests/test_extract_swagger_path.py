c7ba089ccd399aa59f619b9ee511304ec68cd9d4
Seonaid Lee <seonaidl@gmail.com>
2019-11-18 13:25:48 -0500

    refactor extract_swagger_path tests

--- tests/test_extract_swagger_path.py
+++ tests/test_extract_swagger_path.py
@@ -1,4 +1,5 @@
 from flask_restful_swagger import swagger
+import pytest
 """
   Docstring from extract_swagger_path:
 
@@ -8,23 +9,18 @@ from flask_restful_swagger import swagger
   to this: /{lang_code}/{id}/{probability}
   """
 
-def test_extract_swagger_path_empty_string():
-    assert swagger.extract_swagger_path('') == ''
+@pytest.mark.parametrize("case_name, test_input, expected", 
+    [('empty_string', '', ''), 
+    ('simple', '/endpoint', '/endpoint'),
+    ('single_parameter_no_type', '/path/<parameter>', '/path/{parameter}'),
+    ('single_parameter_string', '/<string(length=2):lang_code>', '/{lang_code}'),
+    ('multiple_parameters', '/<string(length=3):lang_code>/<string:id>/<float:probability>', '/{lang_code}/{id}/{probability}'),
+    ('multiple_parameters_varied_length_string', '/<string(length=5):lang_code>/<string:id>/<float:probability>', '/{lang_code}/{id}/{probability}'),
+    ('long_path_single_parameter', 'path/subpath/other_path/<string(length=2):lang_code>', 'path/subpath/other_path/{lang_code}')])
 
-def test_extract_swagger_path_simple():
-    assert swagger.extract_swagger_path('/endpoint') == '/endpoint'
+def test_extract_swagger_path(case_name, test_input, expected):
+    assert swagger.extract_swagger_path(test_input) == expected
 
 def test_extract_swagger_path_returns_string():
     assert isinstance(swagger.extract_swagger_path('/endpoint/123'), str)
 
-def test_extract_swagger_path_single_parameter_no_type():
-    assert swagger.extract_swagger_path('/path/<parameter>') == '/path/{parameter}'
-
-def test_extract_swagger_path_single_parameter_string():
-    assert swagger.extract_swagger_path('/<string(length=2):lang_code>') == '/{lang_code}'
-
-def test_extract_swagger_path_multiple_parameters():
-    assert swagger.extract_swagger_path('/<string(length=2):lang_code>/<string:id>/<float:probability>') == '/{lang_code}/{id}/{probability}'
-
-def test_extract_swagger_path_long_path_single_parameter():
-    assert swagger.extract_swagger_path('path/subpath/other_path/<string(length=2):lang_code>') == 'path/subpath/other_path/{lang_code}'
