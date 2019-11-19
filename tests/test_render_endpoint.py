40005cb735560fd8cf332c7d3240a5218f9409b9
Seonaid Lee <seonaidl@gmail.com>
2019-11-19 09:55:10 -0500

    linting the files

--- tests/test_render_endpoint.py
+++ tests/test_render_endpoint.py
@@ -1,15 +1,20 @@
-from flask_restful_swagger import swagger
 import unittest
 from unittest.mock import patch
 
-class Endpoint():
+from flask_restful_swagger import swagger
+
+
+class Endpoint:
     pass
 
-class RenderEndpointTestCast(unittest.TestCase):
 
+class RenderEndpointTestCast(unittest.TestCase):
     def test_render_endpoint(self):
         endpoint = Endpoint()
-        with patch ('flask_restful_swagger.swagger.render_page') as mock_render_page:
+        with patch(
+            "flask_restful_swagger.swagger.render_page"
+        ) as mock_render_page:
             swagger.render_endpoint(endpoint)
-            mock_render_page.assert_called_with("endpoint.html", endpoint.__dict__)
-
+            mock_render_page.assert_called_with(
+                "endpoint.html", endpoint.__dict__
+            )
