794c6e09ec92477665b7af87f5e2f49a35abd08f
David Lieu <torontodavid999@gmail.com>
2019-11-19 09:49:15 -0500

    testing: Run linting tools on test_model and test_operation

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,24 +1,26 @@
 import datetime
-import pytest
-from flask_restful_swagger import swagger
 from unittest.mock import patch
 
+import pytest
 
-class TestEmptyClass():
+from flask_restful_swagger import swagger
+
+
+class TestEmptyClass:
     pass
 
 
-@pytest.mark.parametrize("test_input", [
-                                TestEmptyClass,  #Test a class
-                                'im a str',  #Test a str
-                                123,  #Test int
-                                None,  #Test None
-                                datetime.datetime.now(),  #Test datetime
-                                ])
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        TestEmptyClass,  # Test a class
+        "im a str",  # Test a str
+        123,  # Test int
+        None,  # Test None
+        datetime.datetime.now(),  # Test datetime
+    ],
+)
 def test_model_with_input(test_input):
-    with patch('flask_restful_swagger.swagger.add_model') as mock_add_model:
+    with patch("flask_restful_swagger.swagger.add_model") as mock_add_model:
         assert swagger.model(test_input) == test_input
         mock_add_model.assert_called_once_with(test_input)
-
-
-
