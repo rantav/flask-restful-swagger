bd71db58223f3bfad444c9b2bb3a699d4dbaa974
Niall Byrne <niall@niallbyrne.ca>
2019-11-22 20:39:46 -0500

    tests: refactor duplicated test cases

--- tests/test_render_hompage.py
+++ tests/test_render_hompage.py
@@ -1,19 +1,14 @@
-from unittest.mock import patch
-
-from flask import Response
-
 from flask_restful_swagger import swagger
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
 
 @patch('flask_restful_swagger.swagger.render_page')
 @patch('flask.wrappers.Response')
 def test_render_hompage_func(response_obj, mock_render_page):
     resource_list_url = "resource_list_url"
-    mock_render_page.return_value = Response()
     swagger.render_homepage(resource_list_url)
     mock_render_page.assert_called_once_with("index.html", {"resource_list_url": resource_list_url})
-
-
-
-# def render_homepage(resource_list_url):
-#     conf = {"resource_list_url": resource_list_url}
-# #     return render_page("index.html", conf)
--- tests/test_sanitize_doc.py
+++ tests/test_sanitize_doc.py
@@ -1,57 +1,18 @@
 import pytest
 from flask_restful_swagger import swagger
 
+
 @pytest.mark.parametrize(
     "test_input,expected",
-    [("Hey\n", "Hey<br/>"),
-     ("\n/n\n/n\n", "<br/>/n<br/>/n<br/>")]
+    [
+        ("Hey\n", "Hey<br/>"),
+        ("\n/n\n/n\n", "<br/>/n<br/>/n<br/>"),
+        ("No Change", "No Change")
+     ]
 )
 def test_string_sanitize_doc(test_input, expected):
-    """Uses string cases"""
     assert swagger._sanitize_doc(test_input) == expected
 
 
-@pytest.mark.parametrize(
-    "test_input_types, expected_output",
-    [(str(12345) + "\n", "12345<br/>"),
-     (str(23+45+31.689923) + "\n", "99.689923<br/>"),
-     (str((2 / 1)) + str(2 % 1) + "\/\/\n/\\", "2.00\/\/<br/>/\\")]
-)
-def test_numbers_sanitize_doc(test_input_types, expected_output):
-    assert swagger._sanitize_doc(test_input_types) == expected_output
-
-
-def test_purenum_sanitize_doc():
-    try:
-        numbers = 6736+32843.73828932+0.138238
-        sentence = "I was able to do this a "
-        ending = " times.\nWhat do you think?"
-        assert isinstance(swagger._sanitize_doc(sentence + numbers + ending), TypeError)
-    except TypeError:
-        assert True
-
-
-def test_zero_errors_sanitize_doc():
-    try:
-        sentence = "I was able to make $"
-        numbers = 99999999999/0
-        ending = "\np/m"
-        assert isinstance(swagger._sanitize_doc(sentence + numbers + ending), ZeroDivisionError)
-    except ZeroDivisionError:
-        assert True
-
-
 def test_none_sanitize_doc():
-    try:
-        none_var = None
-        assert isinstance(swagger._sanitize_doc(none_var), AssertionError)
-    except AssertionError:
-        assert True
-
-
-@pytest.mark.parametrize(
-    "test_input_types, expected_output",
-    [("\r\n\'\/\n/'n", "\r<br/>'\\/<br/>/'n")]
-)
-def test_escape_characters_sanitize_doc(test_input_types, expected_output):
-    assert swagger._sanitize_doc(test_input_types) == expected_output
+    assert swagger._sanitize_doc(None) is None
